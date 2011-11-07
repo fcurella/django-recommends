5ec73c875e477e5c9a2763bd32343a415f863f35
Flavio Curella <flavio.curella@gmail.com>
2011-11-07 09:09:51 -0600

    fide import errore in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-from myapp import VERSION
+from recommends import VERSION
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
