74d3101b6f3a700fd88b338dbb785eca54bef766
Flavio Curella <flavio.curella@gmail.com>
2011-11-15 16:31:27 -0600

    fixed template tags

--- recommends/templatetags/recommends.py
+++ recommends/templatetags/recommends.py
@@ -1,25 +1,24 @@
 from ..models import SimilarityResult, Recommendation
-from django.contrib.sites import Site
+from django.contrib.sites.models import Site
 from django.db import models
 from django import template
 register = template.Library()
 
 
 @register.filter
-def similars(obj, limit):
+def similarities(obj, limit=5):
     """
     Returns a list of SimilarityResult, representing how much an object is similar to the given one.
 
     Usage::
 
-        {% for similar in myobj|similars:5 %}
-            {{ similar.get_object }}
+        {% for similarities in myobj|similar:5 %}
+            {{ similarities.get_object }}
         {% endfor %}
     """
     if isinstance(obj, models.Model):
         object_site = Site.objects.get_current()
-
-        return SimilarityResult.objects.similar_to(obj, site=object_site)[:int(limit)]
+        return SimilarityResult.objects.similar_to(obj, site=object_site, score__gt=0)[:int(limit)]
 
 
 class SuggestionNode(template.Node):
@@ -31,12 +30,12 @@ class SuggestionNode(template.Node):
     def render(self, context):
         user = context['user']
         if user.is_authenticated():  # We need an id after all
-            suggestions = Recommendation.objects.get_recommendations_for_user(user, self.site)[:self.limit]
+            suggestions = Recommendation.objects.filter(user=user, object_site=self.site)[:self.limit]
             context[self.varname] = suggestions
         return ''
 
 
-@register.simple_tag(takes_context=True)
+@register.tag()
 def suggested(parser, token):
     """
     Returns a list of Recommendation (suggestions of objects) for the current user.
@@ -47,9 +46,9 @@ def suggested(parser, token):
     {% endfor %}
     """
     bits = token.contents.split()
-    varname = bits[3]
+    varname = bits[2]
     try:
-        limit = int(bits[5])
+        limit = int(bits[4])
     except IndexError:
         limit = 5
     return SuggestionNode(varname, limit)
