217938bde38f56d682e94f7f197d442f357f7e4b
Flavio Curella <flavio.curella@gmail.com>
2011-11-15 10:49:21 -0600

    instantiate provider before calling its methods

--- recommends/tasks.py
+++ recommends/tasks.py
@@ -6,6 +6,7 @@ from .providers import recommendation_registry
 
 @periodic_task(run_every=crontab(hour="*/24"))
 def precompute_items():
-    for provider in recommendation_registry.providers:
-        prefs = provider.prefs()
-        provider.precompute(prefs)
+    for Provider in recommendation_registry.providers:
+        provider_instance = Provider()
+        prefs = provider_instance.prefs()
+        provider_instance.precompute(prefs)
