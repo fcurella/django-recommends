f6b393f4cf3a1fcd44f65ae8338cef81c6428692
Flavio Curella <flavio.curella@gmail.com>
2011-11-15 12:34:59 -0600

    renamed task

--- recommends/tasks.py
+++ recommends/tasks.py
@@ -5,7 +5,7 @@ from .providers import recommendation_registry
 
 
 @periodic_task(run_every=crontab(hour="*/24"))
-def precompute_items():
+def precompute():
     for Provider in recommendation_registry.providers:
         provider_instance = Provider()
         prefs = provider_instance.prefs()
