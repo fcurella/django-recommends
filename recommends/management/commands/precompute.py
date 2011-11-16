fbb416239a8a81e0f50131b5ba525ad3a61cabe1
Flavio Curella <flavio.curella@gmail.com>
2011-11-16 13:44:06 -0600

    delete spurious command

--- recommends/management/commands/precompute.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import NoArgsCommand
-from recommends.tasks import recommends_precompute
-
-
-class Command(NoArgsCommand):
-    def handle(self, *args, **options):
-        recommends_precompute()
