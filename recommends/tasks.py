2ff906add0d119a7f40da82eeee1b412114937b4
Flavio Curella <flavio.curella@gmail.com>
2011-11-17 14:30:51 -0600

    added a performance hack to tasks.py

--- recommends/tasks.py
+++ recommends/tasks.py
@@ -7,7 +7,9 @@ from .settings import RECOMMENDS_TASK_CRONTAB
 
 @periodic_task(run_every=crontab(**RECOMMENDS_TASK_CRONTAB))
 def recommends_precompute():
-    for Provider in recommendation_registry.providers:
-        provider_instance = Provider()
+    # I know this is weird, but it's faster (tested on CPyhton 2.6.5)
+    def _precompute(ProviderClass):
+        provider_instance = ProviderClass()
         prefs = provider_instance.prefs()
         provider_instance.precompute(prefs)
+    [_precompute(Provider) for Provider in recommendation_registry.providers]
