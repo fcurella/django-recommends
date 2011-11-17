327b5b6e60cb0c313f7d64fe640a51c4aacd1039
Flavio Curella <flavio.curella@gmail.com>
2011-11-17 15:24:05 -0600

    removed unused methods in managers

--- recommends/managers.py
+++ recommends/managers.py
@@ -16,34 +16,6 @@ class SimilarityResultManager(RecommendsManager):
     def get_query_set(self):
         return super(RecommendsManager, self).get_query_set().filter(score__isnull=False)
 
-    def prefs_for_qs(self):
-        """
-        Returns a dict representing similarity scores for a given content type::
-
-        {
-            "<object_id>": [
-                            (<score>, <related_object_id>),
-                            (<score>, <related_object_id>),
-            ],
-            "<object_id>": [
-                            (<score>, <related_object_id>),
-                            (<score>, <related_object_id>),
-            ],
-        }
-
-        """
-        prefs = {}
-        for result in self.get_query_set():
-            item_key = result.object_identifier()
-            related_key = result.related_object_identifier()
-            score = result.score
-            prefs.setdefault(item_key, [])
-            prefs[item_key].append((score, related_key))
-        return prefs
-
-    def prefs_for_model(self, model):
-        return self.filter_for_model(model).prefs()
-
     def get_or_create_for_objects(self, object_target, object_target_site, object_related, object_related_site):
         object_ctype = ContentType.objects.get_for_model(object_target)
         object_id = object_target.id
