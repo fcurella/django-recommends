f8f6247767abccc8b78882acfdde9af3f1240cfa
Flavio Curella <flavio.curella@gmail.com>
2012-02-02 09:49:23 -0600

    made dangoormstorage cache types for better performance

--- recommends/storages/djangoorm/managers.py
+++ recommends/storages/djangoorm/managers.py
@@ -4,9 +4,25 @@ from django.contrib.contenttypes.models import ContentType
 
 
 class RecommendsManager(models.Manager):
+    _ctypes = None
+
+    @property
+    def ctypes(self):
+        if self._ctypes is None:
+            values = ContentType.objects.values_list('app_label', 'model', 'id')
+            ctypes = {}
+            [ctypes.update({"%s.%s" % x[:2]: x[2]}) for x in values]
+            self._ctypes = ctypes
+        return self._ctypes
+
+    def get_ctype_id_for_obj(self, obj):
+        app_label = obj._meta.app_label
+        module_name = obj._meta.module_name
+        return self.ctypes["%s.%s" % (app_label, module_name)]
+
     def filter_for_model(self, model):
-        ctype = ContentType.objects.get_for_model(model)
-        return self.filter(object_ctype=ctype.id)
+        ctype_id = self.get_ctype_id_for_obj(model)
+        return self.filter(object_ctype=ctype_id)
 
     def filter_for_object(self, obj):
         return self.filter_for_model(obj).filter(object_id=obj.id)
@@ -24,17 +40,17 @@ class SimilarityManager(RecommendsManager):
         return super(SimilarityManager, self).get_query_set().filter(score__isnull=False)
 
     def get_or_create_for_objects(self, object_target, object_target_site, object_related, object_related_site):
-        object_ctype = ContentType.objects.get_for_model(object_target)
+        object_ctype_id = self.get_ctype_id_for_obj(object_target)
         object_id = object_target.id
 
-        related_object_ctype = ContentType.objects.get_for_model(object_related)
+        related_object_ctype_id = self.get_ctype_id_for_obj(object_related)
         related_object_id = object_related.id
 
         return self.get_or_create(
-            object_ctype=object_ctype.id,
+            object_ctype=object_ctype_id,
             object_id=object_id,
             object_site=object_target_site.id,
-            related_object_ctype=related_object_ctype.id,
+            related_object_ctype=related_object_ctype_id,
             related_object_id=related_object_id,
             related_object_site=object_related_site.id
         )
@@ -56,11 +72,11 @@ class RecommendationManager(RecommendsManager):
         return super(RecommendationManager, self).get_query_set().filter(score__isnull=False)
 
     def get_or_create_for_object(self, user, object_recommended, object_site):
-        object_ctype = ContentType.objects.get_for_model(object_recommended)
+        object_ctype_id = self.get_ctype_id_for_obj(object_recommended)
         object_id = object_recommended.id
 
         return self.get_or_create(
-            object_ctype=object_ctype.id,
+            object_ctype=object_ctype_id,
             object_id=object_id,
             object_site=object_site.id,
             user=user.id
