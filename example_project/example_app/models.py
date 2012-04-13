712c278ba6ffa704fed095626e1f34f704e89235
Flavio Curella <flavio.curella@gmail.com>
2012-04-13 11:46:48 -0500

    removed cruft

--- example_project/example_app/models.py
+++ example_project/example_app/models.py
@@ -1,7 +1,1 @@
-from django.db import models
-from django.contrib.auth.models import User
-from django.contrib.sites.models import Site
-from recommends.providers import recommendation_registry, RecommendationProvider
-
-
 # Create your models here.
