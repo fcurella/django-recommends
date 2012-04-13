27cffeaa82fe408d1fafbd6d261d63eaf555e75c
Flavio Curella <flavio.curella@gmail.com>
2012-04-13 11:06:45 -0500

    removed reference to a not-yet-existing Algorithm

--- example_project/example_app/models.py
+++ example_project/example_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from recommends.providers import recommendation_registry, RecommendationProvider
-from recommends.algorithms.pyrecsys import RecSysAlgorithm
 
 
 # Create your models here.
