343e9e688d43d77eabc55bd8f99ea7a2b9e193d1
wgx731 <wgx731@gmail.com>
2013-07-04 16:11:01 +0800

    Clean settings.py format
    accroding to flake8

--- recommends/tests/settings.py
+++ recommends/tests/settings.py
@@ -156,15 +156,15 @@ BROKER_URL = 'redis://localhost:6379/0'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-    # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': ':memory:',
-    # Or path to database file if using sqlite3.
+        # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',
-    # Set to empty string for localhost. Not used with sqlite3.
+        # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',
-    # Set to empty string for default. Not used with sqlite3.
+        # Set to empty string for default. Not used with sqlite3.
     }
 }
 
