3bcd5a02599b65b03e08c5f2e43ad789052b98b1
Flavio Curella <flavio.curella@gmail.com>
2013-11-16 10:59:42 -0600

    fix indentation

--- recommends/tests/urls.py
+++ recommends/tests/urls.py
@@ -1,7 +1,7 @@
 try:
-  from django.conf.urls.defaults import patterns, include, url
+    from django.conf.urls.defaults import patterns, include, url
 except ImportError:
-  from django.conf.urls import patterns, include, url
+    from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView, DetailView
 from .models import RecProduct
 
@@ -11,22 +11,22 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       # Examples:
-                       # url(r'^$', 'example_project.views.home', name='home'),
-                       # url(r'^example_project/',
-                       # include('example_project.foo.urls')),
+                        # Examples:
+                        # url(r'^$', 'example_project.views.home', name='home'),
+                        # url(r'^example_project/',
+                        # include('example_project.foo.urls')),
 
-                       # Uncomment the admin/doc line below to enable admin documentation:
-                       # url(r'^admin/doc/',
-                       # include('django.contrib.admindocs.urls')),
+                        # Uncomment the admin/doc line below to enable admin documentation:
+                        # url(r'^admin/doc/',
+                        # include('django.contrib.admindocs.urls')),
 
-                       # Uncomment the next line to enable the admin:
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^login/',
-                           'recommends.tests.views.login', name='login'),
-                       url(r'^product/(?P<pk>\d+)/$',
-                           DetailView.as_view(
-                               model=RecProduct), name='product_detail'),
-                       url(r'^$', TemplateView.as_view(
-                           template_name='home.html'), name='home'),
-                       )
+                        # Uncomment the next line to enable the admin:
+                        url(r'^admin/', include(admin.site.urls)),
+                        url(r'^login/',
+                            'recommends.tests.views.login', name='login'),
+                        url(r'^product/(?P<pk>\d+)/$',
+                            DetailView.as_view(
+                                model=RecProduct), name='product_detail'),
+                        url(r'^$', TemplateView.as_view(
+                            template_name='home.html'), name='home'),
+                        )
