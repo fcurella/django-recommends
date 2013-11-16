de3c132454f3507bd7ac25b9e96f343f2c911456
Flavio Curella <flavio.curella@gmail.com>
2013-11-16 11:20:17 -0600

    make pyflake8 happy

--- recommends/tests/urls.py
+++ recommends/tests/urls.py
@@ -11,22 +11,22 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                        # Examples:
-                        # url(r'^$', 'example_project.views.home', name='home'),
-                        # url(r'^example_project/',
-                        # include('example_project.foo.urls')),
+    # Examples:
+    # url(r'^$', 'example_project.views.home', name='home'),
+    # url(r'^example_project/',
+    # include('example_project.foo.urls')),
 
-                        # Uncomment the admin/doc line below to enable admin documentation:
-                        # url(r'^admin/doc/',
-                        # include('django.contrib.admindocs.urls')),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/',
+    # include('django.contrib.admindocs.urls')),
 
-                        # Uncomment the next line to enable the admin:
-                        url(r'^admin/', include(admin.site.urls)),
-                        url(r'^login/',
-                            'recommends.tests.views.login', name='login'),
-                        url(r'^product/(?P<pk>\d+)/$',
-                            DetailView.as_view(
-                                model=RecProduct), name='product_detail'),
-                        url(r'^$', TemplateView.as_view(
-                            template_name='home.html'), name='home'),
-                        )
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/',
+        'recommends.tests.views.login', name='login'),
+    url(r'^product/(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=RecProduct), name='product_detail'),
+    url(r'^$', TemplateView.as_view(
+        template_name='home.html'), name='home'),
+)
