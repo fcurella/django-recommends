a57dcb4092889abbd1ebee4177905b7913cee78e
Flavio Curella <flavio.curella@gmail.com>
2016-05-31 12:17:36 -0500

    Revert "fix PEP8 issues"
    This reverts commit ea3403429431758a54e62268709783c4f156ee49.

--- recommends/tasks.py
+++ recommends/tasks.py
@@ -1,10 +1,5 @@
-from django.apps import apps
-
 from celery.task import task, periodic_task
 from celery.schedules import crontab
-
-from recommends.providers import recommendation_registry
-
 from .utils import filelock
 
 from .settings import RECOMMENDS_TASK_RUN, RECOMMENDS_TASK_CRONTAB, RECOMMENDS_TASK_EXPIRES
@@ -12,6 +7,7 @@ from .settings import RECOMMENDS_TASK_RUN, RECOMMENDS_TASK_CRONTAB, RECOMMENDS_T
 
 def recommends_precompute():
     results = []
+    from .providers import recommendation_registry
 
     # I know this is weird, but it's faster (tested on CPyhton 2.6.5)
     def _precompute(provider_instance):
@@ -26,10 +22,8 @@ def recommends_precompute():
                 recommendation_registry.storage.release_lock()
     else:
         with filelock('recommends_precompute.lock'):
-            [
-                _precompute(provider_instance)
-                for provider_instance in recommendation_registry.get_vote_providers()
-            ]
+            [_precompute(provider_instance)
+             for provider_instance in recommendation_registry.get_vote_providers()]
 
     return results
 
@@ -41,6 +35,9 @@ if RECOMMENDS_TASK_RUN:
 
 @task(name='remove_suggestions')
 def remove_suggestions(rated_model, object_id):
+    from django.apps import apps
+    from recommends.providers import recommendation_registry
+
     ObjectClass = apps.get_model(*rated_model.split('.'))
     provider_instance = recommendation_registry.get_provider_for_content(
         ObjectClass)
@@ -51,6 +48,9 @@ def remove_suggestions(rated_model, object_id):
 
 @task(name='remove_similarities')
 def remove_similarities(rated_model, object_id):
+    from django.apps import apps
+    from recommends.providers import recommendation_registry
+
     ObjectClass = apps.get_model(*rated_model.split('.'))
     provider_instance = recommendation_registry.get_provider_for_content(
         ObjectClass)
