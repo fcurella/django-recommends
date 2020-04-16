26525a661c7952000dec9cfa1b53d00ab4b321b6
fcurella <flavio.curella@gmail.com>
2020-04-16 09:27:23 -0500

    Bump version: 0.4.0 → 1.0.0

--- recommends/__init__.py
+++ recommends/__init__.py
@@ -1,4 +1,4 @@
-_version = "0.4.0"
+_version = "1.0.0"
 __version__ = VERSION = tuple(map(int, _version.split('.')))
 
 default_app_config = 'recommends.apps.RecommendsConfig'
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION = "0.4.0"
+VERSION = "1.0.0"
 
 
 def read(fname):
